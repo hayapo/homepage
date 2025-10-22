31308f95b796580b2a331fd088c4aaf2752d3e94
hayapo <39045817+hayapo@users.noreply.github.com>
2025-10-23 02:04:51 +0900

    feat: ダークテーマでのちらつきを解消した

--- app/initTheme.ts
+++ app/initTheme.ts
@@ -1,4 +1,25 @@
-import { initTheme, mediaQueryListener } from "./util/theme"
+const mediaQueryListener = (e: MediaQueryListEvent) => {
+	if (localStorage.theme === 'system') {
+		if (e.matches) {
+			document.documentElement.classList.add('dark')
+		} else {
+			document.documentElement.classList.remove('dark')
+		}
+	}
+}
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', mediaQueryListener)
+const initTheme = () => {
+	if (!('theme' in localStorage) || localStorage.theme === 'system') {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.documentElement.classList.add('dark')
+		}
+		localStorage.theme = 'system'
+	} else if (localStorage.theme === 'dark') {
+		document.documentElement.classList.add('dark')
+	} else {
+		document.documentElement.classList.remove('dark')
+	}
+}
+
 initTheme()
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', mediaQueryListener)
--- app/util/theme.ts
+++ app/util/theme.ts
@@ -1,21 +1,6 @@
 // https://github.com/azukiazusa1/sapper-blog-app/blob/main/app/src/utils/darkTheme.ts
 export type Theme = 'system' | 'light' | 'dark'
 
-export const initTheme = () => {
-	if (!('theme' in localStorage) || localStorage.theme === 'system') {
-		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-			document.documentElement.setAttribute('data-theme', 'dark')
-		} else {
-			document.documentElement.setAttribute('data-theme', 'light')
-		}
-		localStorage.theme = 'system'
-	} else if (localStorage.theme === 'dark') {
-		document.documentElement.setAttribute('data-theme', 'dark')
-	} else {
-		document.documentElement.setAttribute('data-theme', 'light')
-	}
-}
-
 export const getTheme = (): Theme => {
 	if (!('theme' in localStorage)) {
 		return 'system'
@@ -33,28 +18,18 @@ export const changeTheme = (value: Theme) => {
 		case 'system':
 			localStorage.setItem('theme', 'system')
 			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-				document.documentElement.setAttribute('data-theme', 'dark')
+				document.documentElement.classList.add('dark')
 			} else {
-				document.documentElement.setAttribute('data-theme', 'light')
+				document.documentElement.classList.remove('dark')
 			}
-			break
-		case 'light':
-			localStorage.setItem('theme', 'light')
-			document.documentElement.setAttribute('data-theme', 'light')
 			break
 		case 'dark':
 			localStorage.setItem('theme', 'dark')
-			document.documentElement.setAttribute('data-theme', 'dark')
+			document.documentElement.classList.add('dark')
 			break
-	}
-}
-
-export const mediaQueryListener = (e: MediaQueryListEvent) => {
-	if (localStorage.theme === 'system') {
-		if (e.matches) {
-			document.documentElement.setAttribute('data-theme', 'dark')
-		} else {
-			document.documentElement.setAttribute('data-theme', 'light')
-		}
+		case 'light':
+			localStorage.setItem('theme', 'light')
+			document.documentElement.classList.remove('dark')
+			break
 	}
 }
